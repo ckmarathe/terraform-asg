d6d1b09adf8f2b4acd9a2a9f9808371ccb498ebc
Chetan Marathe <ckmarathe0326@gmail.com>
2022-06-01 21:29:35 +0530

    updated SG config

--- asg.tf
+++ asg.tf
@@ -77,13 +77,13 @@ module "asg" {
       delete_on_termination = true
       description           = "eth0"
       device_index          = 0
-      security_groups       = module.sg.security_group_id
+      security_groups       = [module.sg.security_group_id]
     },
     {
       delete_on_termination = true
       description           = "eth1"
       device_index          = 1
-      security_groups       = module.sg.security_group_id
+        security_groups       = [module.sg.security_group_id]
     }
   ]
 
