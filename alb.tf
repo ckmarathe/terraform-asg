f71e1fd2a6d04976de1d5d5d4710258605a43c6c
Chetan Marathe <ckmarathe0326@gmail.com>
2022-06-01 21:38:28 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -8,7 +8,7 @@ module "alb" {
 
   vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
-  security_groups    = [[module.sg.security_group_id]
+  security_groups    = [module.sg.security_group_id]
 
   access_logs = {
     bucket = "my-alb-logs"
